9426b950ecf7fc21472ed012f6fa88fe5e08b5a2
Tyler Kennedy <tk@tkte.ch>
2024-03-28 04:32:45 -0400

    Improve test coverage for workflow graph creation.

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -29,3 +29,74 @@ def test_workflow_serialize():
     deserialized = Workflow.deserialize(serialized)
 
     assert deserialized.serialize() == serialized
+
+
+def test_workflow_dependencies():
+    """
+    Ensure we can get the dependencies for each node in the graph.
+    """
+    workflow = Workflow(
+        Chain(
+            dummy_job(),
+            Group(
+                dummy_job(),
+                dummy_job(),
+            ),
+        )
+    )
+
+    assert list(workflow.dependencies) == [
+        ("1", []),
+        ("2", ["1"]),
+        ("3", ["1"]),
+    ]
+
+
+def test_workflow_chain_before_group():
+    """
+    Ensure we can chain a job before a group.
+    """
+    workflow = Workflow(
+        Chain(
+            Chain(
+                dummy_job(),
+                dummy_job(),
+            ),
+            Group(
+                dummy_job(),
+                dummy_job(),
+            ),
+        )
+    )
+
+    assert list(workflow.dependencies) == [
+        ("1", []),
+        ("2", ["1"]),
+        ("3", ["2"]),
+        ("4", ["2"]),
+    ]
+
+
+def test_workflow_group_before_chain():
+    """
+    Ensure we can group jobs before a chain.
+    """
+    workflow = Workflow(
+        Chain(
+            Group(
+                dummy_job(),
+                dummy_job(),
+            ),
+            Chain(
+                dummy_job(),
+                dummy_job(),
+            ),
+        )
+    )
+
+    assert list(workflow.dependencies) == [
+        ("1", []),
+        ("2", []),
+        ("3", ["1", "2"]),
+        ("4", ["3"]),
+    ]
